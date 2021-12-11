8b1e983c35d3b6ef702ff805be6df8bd1901ebe0
d798 <you@example.comd798asa@gmail.com>
2021-12-11 16:07:03 -0500

    first react app

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
+import ErrorBoundry from '../components/ErrorBoundry';
 
 
 
@@ -49,7 +50,9 @@ class App extends React.Component {
 
 
                     <Scroll>
-                        <CardList robots={filtterRobot} />
+                        <ErrorBoundry>
+                            <CardList robots={filtterRobot} />
+                        </ErrorBoundry>
                     </Scroll>
 
                 </React.Fragment>
